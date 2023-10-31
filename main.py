88501ced1a582bdcc3a0283788f9ceea0db8b661
samha <samha@student.chalmers.se>
2023-10-31 19:54:41 +0000

    #3 - implements logging

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import settings
 import discord
 from discord.ext import commands
 
+logger = settings.logging.getLogger("bot")
 
 def run():
     intents = discord.Intents.default()
@@ -10,16 +11,15 @@ def run():
 
     @bot.event
     async def on_ready():
-        print(bot.user)
-        print(bot.user.id)
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+
 
     @bot.command()
     async def ping(ctx):
         await ctx.send('pong')
 
 
-    bot.run(settings.DISCORD_API_SECRET)
-    print(settings.DISCORD_API_SECRET)
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
 if __name__ == '__main__':
     run()
